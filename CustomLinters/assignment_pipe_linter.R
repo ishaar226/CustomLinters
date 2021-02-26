da1b755ffc6cf4e4bb7a00fe88d44578671658a3
ishaar6 <40862088+ishaar6@users.noreply.github.com>
2021-02-25 18:50:11 -0600

    changed the assignment-pipe-linter

--- CustomLinters/assignment_pipe_linter.R
+++ CustomLinters/assignment_pipe_linter.R
@@ -1,19 +1,24 @@
+# assignment_pipe_linter.R 
+# Created on 2021-02-25
+# Created by Ishaar Ganesan
 #' @desribeIn linter checks for assignment pipes
 #' @export
-# test change
 
-assignment_pipe_linter <- function(source_file) {
-  lapply(ids_with_token(source_file, " %<>% "),
-         function(id) {
-           parsed <- source_file$parsed_content[id,]
-           Lint(
-             filename = source_file$filename,
-             line_number = parsed$filename,
-             line_number = parsed$line1, 
-             column_number = parsed$col1,
-             type = "style",
-             message = "Assignment pipes should not be used.",
-             line = source_file$lines[parsed$line1]
-           )
-         })
+assignment_pipe_linter <- function() {
+  Linter(function(source_file) {
+  lapply(
+    ids_with_token(source_file,"SPECIAL"),
+    function(id) {
+      parsed <- source_file$parsed_content[id,]
+      Lint(
+        filename = source_file$filename,
+        line_number = parsed$line1, 
+        column_number = parsed$col1,
+        type = "style",
+        message = "Assignment pipes should not be used.",
+        line = source_file$lines[parsed$line1]
+       )
+      }
+    )
+  })
 }
--- CustomLinters/tests/test-assignment_pipe_linter.R
+++ CustomLinters/tests/test-assignment_pipe_linter.R
@@ -1,8 +1,9 @@
-context("assignment_pipe_linter")
-source(here("CustomLinters/assignment_pipe_linter.R"))  
 library(lintr)
 library(testthat)
 library(rex)
+context("assignment_pipe_linter")
+source("CustomLinters/CustomLinters/assignment_pipe_linter.R")  
+
 
 test_that("returns the correct lintting", {
   expect_lint("Blah", NULL, assignment_pipe_linter)
